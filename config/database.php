4881d316d12d5abf4c29e7aa92478cb2e449e56d
Alexmar Ramos Noronha Junior <50970267+AlexmarJr@users.noreply.github.com>
2020-01-30 09:18:58 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'db4free.net1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'systemdb2020'),
             'username' => env('DB_USERNAME', 'cajazeiras'),
